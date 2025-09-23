3f84a4def49a4ec51853fa9d80ae55898bb408ca
mostafa <mostafarasekh5@gmail.com>
2025-09-23 15:58:48 +0300

    connect firebase with project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,6 +8,7 @@ import 'core/helpers/flutter_secure_storge_helper.dart';
 import 'core/utils/bloc_observer.dart';
 import 'core/utils/dependency_injection/di.dart';
 import 'feed_app.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +16,10 @@ void main() async {
   await CacheHelper.init();
   await FlutterSecureStorageHelper.init();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   Bloc.observer = MyBlocObserver();
 
   runApp(
